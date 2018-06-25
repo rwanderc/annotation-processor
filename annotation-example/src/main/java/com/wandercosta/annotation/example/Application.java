71e2f644018b8f50bd22e137c6a37c1aba7c332b
Wander Costa <w.bezerra-da-costa@mytaxi.com>
2018-06-25 12:20:27 +0200

    Enhancing design of processor by adding a model

--- annotation-example/src/main/java/com/wandercosta/annotation/example/Application.java
+++ annotation-example/src/main/java/com/wandercosta/annotation/example/Application.java
@@ -1,6 +1,8 @@
 package com.wandercosta.annotation.example;
 
-import java.util.Arrays;
+import java.util.stream.Stream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class Application {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
     private PersonConfig personConfig;
 
@@ -30,14 +34,15 @@ public class Application {
 
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-        };
+        return args -> logBeans(ctx.getBeanDefinitionNames());
+    }
+
+    private void logBeans(String[] beans) {
+        String log = Stream.of(beans).sorted()
+            .map(bean -> " - " + bean)
+            .reduce((s1, s2) -> s1.concat("\n").concat(s2))
+            .orElse("No beans found! What?");
+        LOG.info("Let's inspect the beans provided by Spring Boot:\n{}", log);
     }
 
 }
--- annotation-lib/src/main/java/com/wandercosta/annotation/ACustomAnnotationProcessor.java
+++ annotation-lib/src/main/java/com/wandercosta/annotation/ACustomAnnotationProcessor.java
@@ -5,7 +5,12 @@ import static java.util.stream.Collectors.toSet;
 import com.google.auto.service.AutoService;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Set;
+import java.util.stream.Stream;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
@@ -15,11 +20,20 @@ import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.tools.JavaFileObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SupportedAnnotationTypes("com.wandercosta.annotation.ACustomAnnotation")
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 @AutoService(Processor.class)
 public class ACustomAnnotationProcessor extends AbstractProcessor {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ACustomAnnotationProcessor.class);
+
+    private static final String OWNER_MODEL_FILE = "com/wandercosta/annotation/ownerBeanModel";
+    private static final String SEPARATOR = System.getProperty("line.separator", "\n");
+    private static final String PACKAGE_PATH_SEPARATOR = ".";
+    private static final String BEAN_MODEL_NAME = "Annotated%sBean";
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
@@ -27,56 +41,55 @@ public class ACustomAnnotationProcessor extends AbstractProcessor {
             Set<? extends Element> annotatedElements = roundEnv.getElementsAnnotatedWith(annotation);
 
             Set<? extends Element> classInterfaceAnnotatedElements = annotatedElements.stream()
-                    .filter(el -> el.getKind().isClass() || el.getKind().isInterface())
-                    .collect(toSet());
+                .filter(el -> el.getKind().isClass() || el.getKind().isInterface())
+                .collect(toSet());
 
             for (Element el : classInterfaceAnnotatedElements) {
                 TypeElement tel = (TypeElement) el;
                 try {
-                    writeBuilderFile(tel.getQualifiedName().toString());
-                } catch (Exception e) {
+                    processClass(tel.getQualifiedName().toString());
+                } catch (Exception ex) {
+                    LOG.error(ex.getMessage(), ex);
+                    throw new RuntimeException(ex);
                 }
             }
         }
         return true;
     }
 
-    private void writeBuilderFile(String className) throws IOException {
-        String packageName = null;
-        int lastDot = className.lastIndexOf('.');
-        if (lastDot > 0) {
-            packageName = className.substring(0, lastDot);
+    private void processClass(String className) throws IOException, URISyntaxException {
+        int lastDot = className.lastIndexOf(PACKAGE_PATH_SEPARATOR);
+        String packagePath = lastDot > 0 ? className.substring(0, lastDot) : null;
+        String shortClassName = className.substring(lastDot + 1);
+        String shortBeanClassName = String.format(BEAN_MODEL_NAME, shortClassName);
+
+        String model = readFile(OWNER_MODEL_FILE);
+        String code = modelWithValues(model, packagePath, shortBeanClassName, shortClassName);
+
+        String fullBeanClassName = packagePath + PACKAGE_PATH_SEPARATOR + shortBeanClassName;
+        writeFile(fullBeanClassName, code);
+    }
+
+    private String modelWithValues(String model, String packagePath, String shortBeanClassName, String shortClassName) {
+        return model
+            .replaceAll("%packageName%", packagePath)
+            .replaceAll("%simpleBeanClassName%", shortBeanClassName)
+            .replaceAll("%simpleClassName%", shortClassName);
+    }
+
+    private String readFile(String fileName) throws URISyntaxException, IOException {
+        Path path = Paths.get(getClass().getClassLoader().getResource(fileName).toURI());
+        try (Stream<String> lines = Files.lines(path)) {
+            return lines
+                .reduce((s1, s2) -> s1.concat(SEPARATOR).concat(s2))
+                .orElseThrow(() -> new IllegalStateException(String.format("Model file '%s' was not found.", fileName)));
         }
+    }
 
-        String simpleClassName = className.substring(lastDot + 1);
-        String beanClassName = className + "Bean";
-        String beanSimpleClassName = beanClassName.substring(lastDot + 1);
-
-        JavaFileObject builderFile = processingEnv.getFiler().createSourceFile(beanClassName);
+    private void writeFile(String path, String code) throws IOException {
+        JavaFileObject builderFile = processingEnv.getFiler().createSourceFile(path);
         try (PrintWriter out = new PrintWriter(builderFile.openWriter())) {
-            if (packageName != null) {
-                out.print("package ");
-                out.print(packageName);
-                out.println(";");
-                out.println();
-            }
-
-            out.println("import org.aeonbits.owner.ConfigFactory;");
-            out.println("import org.springframework.context.annotation.Bean;");
-            out.println("import org.springframework.context.annotation.Configuration;");
-            out.println();
-
-            out.println("@Configuration");
-            out.println("class Annotated" + simpleClassName + " {");
-            out.println();
-
-            out.println("    @Bean");
-            out.println("    " + simpleClassName + " annotated" + simpleClassName + "Bean() {");
-            out.println("        return ConfigFactory.create(" + simpleClassName + ".class);");
-            out.println("    }");
-            out.println();
-
-            out.println("}");
+            out.print(code);
         }
     }
 
